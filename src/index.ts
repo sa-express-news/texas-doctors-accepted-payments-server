76871ec24708890f7479c2e7ef8f3d1b14399b1c
Ubuntu <ubuntu@ip-172-26-6-26.us-east-2.compute.internal>
2018-08-09 00:06:27 +0000

    Added cors support

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,16 @@ require('dotenv').config();
 
 import * as express from 'express';
 import { Express } from 'express';
+import * as cors from 'cors';
 
 import routes from './routes';
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+app.use(cors());
 
 routes(app);
 
+const port = process.env.PORT || 3000;
 app.listen(port);
 
 console.log('RESTful API server running on: ' + port);
